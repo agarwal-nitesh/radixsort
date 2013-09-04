20e9864af50d09ab584bb2e748bb003c0e7b9cf6
erudite <niteshagarwal1.618@gmail.com>
2013-09-04 12:58:11 +0530

    radixSort

--- radixsort.cpp
+++ radixsort.cpp
@@ -7,32 +7,30 @@ using namespace std;
 void radixSort(int list[],int n){
 
   int bucket[n];
-	int digpos=1;
+  int digpos=1;
   int max=list[0];
-
-	for(int i=1;i<n;i++){
-	  if(list[i]>max)
-      max=list[i];
-	}
-
-	while(max/digpos>0){
+  
+  for(int i=1;i<n;i++){
+    if(list[i]>max)
+    max=list[i];
+  }
+  
+  while(max/digpos>0){
+  
+    int digit[10]={0};
     
-		int digit[10]={0};
-
-
-	  for(int i=0;i<n;i++)
-		  digit[(list[i]/digpos)%10]++;
-
-	  for(int i=1;i<10;i++)
-		  digit[i]+=digit[i-1];
-
-		for(int i=n-1;i>=0;i--)
-		  bucket[--digit[(list[i]/digpos)%10]]=list[i];
-		
-		for(int i=0;i<n;i++)
-		  list[i]=bucket[i];
-
-	  digpos*=10;
-	}
+    for(int i=0;i<n;i++)
+      digit[(list[i]/digpos)%10]++;
+      
+    for(int i=1;i<10;i++)
+      digit[i]+=digit[i-1];
+      
+    for(int i=n-1;i>=0;i--)
+      bucket[--digit[(list[i]/digpos)%10]]=list[i];
+      
+    for(int i=0;i<n;i++)
+      list[i]=bucket[i];
+      digpos*=10;
+    }
 
 }
--- test.cpp
+++ test.cpp
@@ -11,64 +11,61 @@ using namespace std;
 
 
 int numdig(int r){
-  
-	int sum=0; 
-	while(r>0){
-	  r/=10;
-		sum++;
-	}
+  int sum=0; 
+  while(r>0){
+    r/=10;
+    sum++;
+  }
   return sum-1;
 }
 
 void print(int* list,int n){
   cout<<endl;
   for(int i=0;i<n;i++){
-	  cout<<setw(NumDigit+1)<<list[i];
-		if((i+1)%15==0)
-		  cout<<endl;
+    cout<<setw(NumDigit+1)<<list[i];
+    if((i+1)%15==0)
+      cout<<endl;
   }
-	cout<<endl;
+  cout<<endl;
 }
 
 int comp(const void* elem1, const void* elem2){
-	if(*(int*)elem1>*(int*)elem2)//for ascending order
-	  return 1;
-  else
-	  return -1;
+
+  if(*(int*)elem1>*(int*)elem2)//for ascending order
+    return 1;
+  else	 
+    return -1;
   
   return 0;
-
 }
 
 void test(int *initarr,int *finalarr){
 
-	qsort(initarr,Size,sizeof(*initarr),comp);
-
-	for(int i=0;i<Size;i++){
-	  if(initarr[i]!=finalarr[i]){
-		  cout<<"Test not passed!..."<<endl;
-	    return;		
-			}
-	}
+  qsort(initarr,Size,sizeof(*initarr),comp);
+  
+  for(int i=0;i<Size;i++){
+    if(initarr[i]!=finalarr[i]){
+      cout<<"Test not passed!..."<<endl;
+      return;		
+    }
+  }
   cout<<"Test Passed"<<endl;
-
 }
 
 int main(){
 
   srand(time(NULL));
-	int list[Size];
+  int list[Size];
   int listcopy[Size];
-	for(int i=0;i<Size;i++){
-	
-	  list[i]=rand()%Range;
-		listcopy[i]=list[i];
-	}
-	cout<<"Before Sorting: "<<endl;
+  for(int i=0;i<Size;i++){
+    list[i]=rand()%Range;
+    listcopy[i]=list[i];
+  }
+  cout<<"Before Sorting: "<<endl;
   print(list,Size);
-
-	radixSort(list,Size);
-	cout<<"After Sorting: "<<endl;
-	print(list,Size);
+  
+  radixSort(list,Size);
+  cout<<"After Sorting: "<<endl;
+  print(list,Size);
   test(listcopy,list);
 }
